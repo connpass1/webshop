3800fbd12f65eecbbb3e0e4582261a32f7b8822d
connpass1 <connpass@gmail.com>
2022-03-18 17:14:42 +0300

    fething

---
+++ src/app/components/Fetching.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FunctionComponent } from "react";
+import { useFetchingId } from "./hooks";
+
+export const CheckFetching: FunctionComponent<{ status: number }> = ({ status, children }) => {
+  if (status > 200)
+    return (
+      <>
+        <h1>Error load data </h1>
+        <h2>{status}</h2>
+      </>
+    );
+  if (status === 200) return <>{children}</>;
+  return <></>;
+};
+export interface ISetData {
+  handleData(data: any): void;
+}
