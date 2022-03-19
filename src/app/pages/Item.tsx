545faf168e11f6986c514647956705aba64d8330
connpass1 <connpass@gmail.com>
2022-03-19 06:45:08 +0300

    github ibit begin

--- src/app/pages/Item.tsx
+++ src/app/pages/Item.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react"; 
 import { ItemLink } from "../components/Elements/ItemLink";
 import { CheckFetching } from "../components/Fetching";
 import { useFetchingId } from "../components/hooks";
